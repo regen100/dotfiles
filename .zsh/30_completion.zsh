7fec2edb4bd74fff91660321bb00c9a936f046d7
Regen <regen100@users.noreply.github.com>
2018-01-14 01:21:23 +0900

    Add lxd completion

--- .zsh/30_completion.zsh
+++ .zsh/30_completion.zsh
@@ -1,2 +1,9 @@
 zstyle ':completion:*' use-cache true
 zstyle ':completion:*:default' menu select=2
+
+if [ -f /usr/share/bash-completion/completions/lxc ]; then
+  autoload bashcompinit
+  bashcompinit
+  export -f _have() { which $@ >/dev/null }
+  source /usr/share/bash-completion/completions/lxc
+fi
