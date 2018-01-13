21e672de07b74a782245538642d194518d6a040c
Regen <regen100@users.noreply.github.com>
2018-01-14 04:06:46 +0900

    Add completion option

--- .zsh/30_completion.zsh
+++ .zsh/30_completion.zsh
@@ -1,6 +1,19 @@
 zstyle ':completion:*' use-cache true
 zstyle ':completion:*:default' menu select=2
 
+zstyle ':completion:*' ignore-parents parent pwd ..
+zstyle ':completion:*:*files' ignored-patterns '*?.o' '*?~' '*\#'
+
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*' completer _expand _complete _match _prefix _approximate _list _history
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' show-completer true
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:manuals' separate-sections true
+zstyle ':completion:*:messages' format '%F{green}%d%f'
+zstyle ':completion:*:warnings' format '%F{red}No matches for:%f %F{yellow}%d%f'
+zstyle ':completion:*:descriptions' format '%F{green}%U%B%d%b%u%f'
+
 if [ -f /usr/share/bash-completion/completions/lxc ]; then
   autoload bashcompinit
   bashcompinit
