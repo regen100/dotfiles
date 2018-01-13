192a0b3c9f2a3daf19c8baeb263a4bc233fb5fa0
Regen <regen100@users.noreply.github.com>
2018-01-14 01:11:25 +0900

    Add alias -s

--- .zsh/40_aliases.zsh
+++ .zsh/40_aliases.zsh
@@ -4,3 +4,10 @@ alias vdir='vdir --color=auto'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
+
+(( $+commands[python3] )) && alias python=python3
+
+alias -s txt='cat'
+(( $+commands[python] )) && alias -s py=python
+(( $+commands[ruby] )) && alias -s rb=ruby
+(( $+commands[aunpack] )) && alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=aunpack
