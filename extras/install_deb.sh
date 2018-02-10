5422ce30606e83bf64a0e66a44f657119598c1e3
Regen <regen100@users.noreply.github.com>
2018-02-11 01:38:37 +0900

    Add universal ctags

---
+++ extras/install_deb.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+package=$(basename "$(pwd)")
+name=${package%-*}
+
+apt-get install --no-install-recommends devscripts equivs
+yes | mk-build-deps -i
+debuild -us -uc -b
+cd ..
+apt-get purge -y --autoremove "$name-build-deps" devscripts equivs
+dpkg -i "$name"*.deb || apt-get -fy install
