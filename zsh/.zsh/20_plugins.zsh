39d1553e790cdaad1f4e68efda260342ab86a25b
Regen <regen100@users.noreply.github.com>
2018-02-18 03:46:12 +0900

    Fix zsh plugin loading

--- zsh/.zsh/20_plugins.zsh
+++ zsh/.zsh/20_plugins.zsh
@@ -8,18 +8,18 @@ zstyle ":zplug:tag" depth 1
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-autosuggestions"
-zplug "chriskempson/base16-shell", hook-load:"base16_default-dark"
+zplug "chriskempson/base16-shell", hook-load:"[[ -L ~/.base16_theme ]] || base16_default-dark"
 zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:fzf
 zplug "junegunn/fzf", as:command, use:bin/fzf-tmux
 zplug "b4b4r07/enhancd", use:init.sh
 zplug "modules/command-not-found", from:prezto
 zplug "mollifier/cd-bookmark", hook-load:"alias cdb=cd-bookmark"
 zplug "docker/compose", use:contrib/completion/zsh, if:"(( $+commands[docker-compose] ))"
-zplug "rust-lang/zsh-config", use:src, if:"(( $+commands[rustc] ))"
+zplug "rust-lang/zsh-config", if:"(( $+commands[rustc] ))"
 zplug "lukechilds/zsh-better-npm-completion", if:"(( $+commands[npm] ))"
 zplug "caarlos0/zsh-mkc"
 zplug "marzocchi/zsh-notify", if:"(( $+commands[xdotool] )) && xdotool getactivewindow > /dev/null"
-zplug 'endaaman/lxd-completion-zsh'
+zplug 'endaaman/lxd-completion-zsh', if:"(( $+commands[lxc] ))"
 
 zplug "plugins/colored-man-pages", from:oh-my-zsh
 zplug "plugins/common-aliases", from:oh-my-zsh
