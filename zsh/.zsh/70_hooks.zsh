1bf30435406f5a2ec7519b4869f3641da98749c1
Regen <regen100@users.noreply.github.com>
2018-02-20 14:38:03 +0900

    Add display hook

--- zsh/.zsh/70_hooks.zsh
+++ zsh/.zsh/70_hooks.zsh
@@ -1,1 +1,9 @@
+autoload -Uz add-zsh-hook
+
 (( $+commands[direnv] )) && eval "$(direnv hook zsh)"
+
+[[ -z $TMUX && -n $DISPLAY ]] && echo $DISPLAY > ~/.display
+function update_display() {
+  [[ -f ~/.display ]] && export DISPLAY="$(cat ~/.display)"
+}
+add-zsh-hook preexec update_display
