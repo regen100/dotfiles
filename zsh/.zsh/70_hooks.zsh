9c047c116c48397fd8d758488e69d178b3d4cb27
Regen <regen100@users.noreply.github.com>
2018-02-21 16:24:55 +0900

    Refactor zsh hooks

--- zsh/.zsh/70_hooks.zsh
+++
@@ -1,9 +0,0 @@
-autoload -Uz add-zsh-hook
-
-(( $+commands[direnv] )) && eval "$(direnv hook zsh)"
-
-[[ -z $TMUX && -n $DISPLAY ]] && echo $DISPLAY > ~/.display
-function update_display() {
-  [[ -f ~/.display ]] && export DISPLAY="$(cat ~/.display)"
-}
-add-zsh-hook preexec update_display
