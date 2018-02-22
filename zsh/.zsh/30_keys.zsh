a86762a5984df377a5751d66e12ddb2165dc5e50
Regen <regen100@users.noreply.github.com>
2018-02-22 11:23:31 +0900

    Add rationalise dot

--- zsh/.zsh/30_keys.zsh
+++ zsh/.zsh/30_keys.zsh
@@ -3,3 +3,12 @@ bindkey -e
 bindkey '^P' history-beginning-search-backward
 bindkey '^N' history-beginning-search-forward
 
+rationalise-dot() {
+  if [[ $LBUFFER = *.. ]]; then
+    LBUFFER+=/..
+  else
+    LBUFFER+=.
+  fi
+}
+zle -N rationalise-dot
+bindkey . rationalise-dot
