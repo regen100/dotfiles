962ceb3711818e3b786be8c84a0d170582f4e571
Regen <regen100@users.noreply.github.com>
2018-02-25 22:23:22 +0900

    Refactor default-enter

--- zsh/.zsh/20_keys.zsh
+++ zsh/.zsh/20_keys.zsh
@@ -14,27 +14,25 @@ rationalise-dot() {
 zle -N rationalise-dot
 bindkey . rationalise-dot
 
+is-in-git() git rev-parse 2>/dev/null
 default-enter() {
-  if [[ -z $BUFFER ]]; then
-    case $[ENTER_COUNT++] in
-      0)
-        BUFFER=" ls_abbrev"
-        ;;
-      1)
-        if git rev-parse 2>/dev/null; then
-          BUFFER=" git status --short --branch"
-        fi
-        ;;
-      2)
-        if git rev-parse 2>/dev/null; then
-          BUFFER=" git --no-pager log --graph --decorate --oneline -n 5"
-        fi
-        ;;
-    esac
-  else
+  if [[ -n $BUFFER ]]; then
     ENTER_COUNT=0
+    zle .accept-line
+    return
   fi
-  [[ -n $BUFFER ]] && zle .accept-line
+
+  local cmd
+  while [[ -z $cmd ]]; do
+    case $[ENTER_COUNT++] in
+      0) cmd="ls_abbrev" ;;
+      1) is-in-git && cmd="git status --short --branch" ;;
+      2) is-in-git && cmd="git --no-pager log --graph --decorate --oneline -n 5" ;;
+      *) return 1 ;;
+    esac
+  done
+  echo && eval $cmd
+  zle reset-prompt
 }
 zle -N default-enter
 bindkey ^m default-enter
