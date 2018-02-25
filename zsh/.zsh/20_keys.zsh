ad1fdac39e5fafb22d992ae478522fd406e19cc7
Regen <regen100@users.noreply.github.com>
2018-02-25 18:04:36 +0900

    Add zle default-enter

--- zsh/.zsh/20_keys.zsh
+++ zsh/.zsh/20_keys.zsh
@@ -12,3 +12,28 @@ rationalise-dot() {
 }
 zle -N rationalise-dot
 bindkey . rationalise-dot
+
+default-enter() {
+  if [[ -z $BUFFER ]]; then
+    case $[ENTER_COUNT++] in
+      0)
+        BUFFER=" ls_abbrev"
+        ;;
+      1)
+        if git rev-parse 2>/dev/null; then
+          BUFFER=" git status --short --branch"
+        fi
+        ;;
+      2)
+        if git rev-parse 2>/dev/null; then
+          BUFFER=" git --no-pager log --graph --decorate --oneline -n 5"
+        fi
+        ;;
+    esac
+  else
+    ENTER_COUNT=0
+  fi
+  [[ -n $BUFFER ]] && zle .accept-line
+}
+zle -N default-enter
+bindkey ^m default-enter
