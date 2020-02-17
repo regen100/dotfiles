0f35f899fd00ccd030d19ab929fca55c15ac8c32
Regen <regen100@users.noreply.github.com>
2020-02-17 11:19:05 +0900

    Add sccache

--- zsh/.zsh/40_aliases.zsh
+++ zsh/.zsh/40_aliases.zsh
@@ -27,11 +27,10 @@ alias mmv='noglob zmv -W'
 alias gdbrun='gdb -ex="set confirm on" -ex=run -ex=quit --args'
 alias valgrindrun='valgrind --exit-on-first-error=yes --error-exitcode=1'
 
-CMAKE_CXX_FLAGS="-fstandalone-debug -fcolor-diagnostics -march=native -ferror-limit=1"
-CMAKE_COMMON_OPTIONS=(-DBUILD_SHARED_LIBS=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=install)
-CMAKE_CCACHE=(-DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache)
-alias -g CMAKE_DEV='"$CMAKE_CCACHE[@]" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" -DCMAKE_C_FLAGS="$CMAKE_CXX_FLAGS" "$CMAKE_COMMON_OPTIONS[@]"'
-alias -g CMAKE_DEV_ZAPCC='-DCMAKE_CXX_COMPILER=zapcc++ -DCMAKE_C_COMPILER=zapcc -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS -isystem/usr/lib/zapcc/7.0.0/include" -DCMAKE_C_FLAGS="$CMAKE_CXX_FLAGS -isystem/usr/lib/zapcc/7.0.0/include" "$CMAKE_COMMON_OPTIONS[@]"'
+CMAKE_CXX_FLAGS="-fstandalone-debug -fcolor-diagnostics -march=native -ferror-limit=1 -g"
+CMAKE_COMMON_OPTIONS=(-DBUILD_SHARED_LIBS=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=install -DBUILD_TESTS=ON)
+CMAKE_SCCACHE=(-DCMAKE_CXX_COMPILER_LAUNCHER=sccache -DCMAKE_C_COMPILER_LAUNCHER=sccache)
+alias -g CMAKE_DEV='"$CMAKE_SCCACHE[@]" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" -DCMAKE_C_FLAGS="$CMAKE_CXX_FLAGS" "$CMAKE_COMMON_OPTIONS[@]"'
 
 (( $+commands[ag] )) && alias todo='ag "//\s*(TODO|FIXME)\b"'
 
