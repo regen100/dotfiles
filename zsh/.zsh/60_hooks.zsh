26ef4504ef279069c02861558d7a77f8e0541dab
Regen <regen100@users.noreply.github.com>
2020-04-30 02:13:19 +0900

    Fix hook

--- zsh/.zsh/60_hooks.zsh
+++ zsh/.zsh/60_hooks.zsh
@@ -7,8 +7,9 @@ autoload -Uz add-zsh-hook
 if [[ -n $BYOBU_TTY ]]; then
   add-zsh-hook preexec update_vars
   update_vars() {
-    [[ "SSH_CONNECTION=$SSH_CONNECTION" != $(tmux show-env SSH_CONNECTION) && -f ~/.base16_theme ]] && source ~/.base16_theme
+    [[ "$CONNECTION" != $(tmux show-env SSH_CONNECTION) && -f ~/.base16_theme ]] && source ~/.base16_theme
     source /usr/bin/byobu-reconnect-sockets
+    CONNECTION=$(tmux show-env SSH_CONNECTION)
   }
 fi
 
