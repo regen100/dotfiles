940026af3ce536a344545ff64e439780906e9406
Regen <regen100@users.noreply.github.com>
2021-03-06 19:36:06 +0900

    Fix zsh

--- zsh/env.sh
+++ zsh/env.sh
@@ -15,6 +15,9 @@ if [ -f $HOME/.cargo/env ]; then
   . $HOME/.cargo/env
 fi
 
+if [ -d $HOME/.local/bin ]; then
+  export PATH="$HOME/.local/bin:$PATH"
+fi
 if [ -d $HOME/bin ]; then
   export PATH="$HOME/bin:$PATH"
 fi
