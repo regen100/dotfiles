e5a931ccf0baf110c0ecb5a8480add48127e38b5
Regen <regen100@users.noreply.github.com>
2022-08-20 20:05:30 +0900

    Fix wsl config

--- zsh/.zshrc.d/60-wsl.zsh
+++ zsh/.zshrc.d/60-wsl.zsh
@@ -1,4 +1,10 @@
-if [[ -z "$DISPLAY" && -n "$WSL_HOST_IP" ]]; then
+if [[ -z $WSL_HOST_IP ]]; then
+  return
+fi
+
+if [[ -z $DISPLAY ]]; then
   export DISPLAY="${WSL_HOST_IP}:0"
   (( $(xauth list "$DISPLAY" 2>&1 | wc -l) )) || xauth generate "$DISPLAY"
 fi
+
+export BROWSER="powershell.exe /C start"
