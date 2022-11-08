ef97ff5229bdb1ae53e954a0e6f48ad4079a8b31
Regen <regen100@users.noreply.github.com>
2022-11-08 11:21:16 +0900

    Remove url-quote

--- zsh/.zshrc.d/20-plugin.zsh
+++ zsh/.zshrc.d/20-plugin.zsh
@@ -13,7 +13,6 @@
 (( $+commands[direnv] )) && source <(direnv hook zsh)
 (( $+commands[keychain] )) && source <(keychain --eval --quiet --inherit any --agents ssh,gpg --ignore-missing --noask id_rsa id_ed25519)
 
-url-quote
 autoload -Uz manydots-magic 2>/dev/null && manydots-magic 2>/dev/null
 
 autoload -Uz bashcompinit && bashcompinit
