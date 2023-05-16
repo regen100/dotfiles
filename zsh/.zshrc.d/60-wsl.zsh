0a315a3d3b1af9cc7f23dd71d68770ba130e44d2
Regen <regen100@users.noreply.github.com>
2023-05-16 19:52:09 +0900

    Add desktop path in WSL

--- zsh/.zshrc.d/60-wsl.zsh
+++ zsh/.zshrc.d/60-wsl.zsh
@@ -9,3 +9,5 @@ if [[ ! -f ~/bin/start ]]; then
   install -m 755 <(echo '#!/bin/bash'; echo 'powershell.exe /C start \""$1"\"') ~/bin/start
 fi
 export BROWSER=$HOME/bin/start
+
+hash -d desktop="$(wslpath "$(/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe /C Write-Output '${Env:USERPROFILE}' | sed 's/\r//')")/Desktop"
