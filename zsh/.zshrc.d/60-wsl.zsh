1f7a272cce74efb8b177c961d5ec8a9ff60804b9
Regen <regen100@users.noreply.github.com>
2024-04-14 21:52:09 +0900

    Fix zsh wsl settings

--- zsh/.zshrc.d/60-wsl.zsh
+++ zsh/.zshrc.d/60-wsl.zsh
@@ -2,12 +2,14 @@ if ! (( $+commands[wslpath] )); then
   return
 fi
 
+readonly powershell="/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
+
 if [[ ! -f ~/bin/start ]]; then
   mkdir -p ~/bin
-  install -m 755 <(echo '#!/bin/bash'; echo 'powershell.exe /C start \""$1"\"') ~/bin/start
+  install -m 755 <(echo '#!/bin/bash'; echo "$powershell"' /C start \""$1"\"') ~/bin/start
 fi
 export BROWSER=$HOME/bin/start
 
-hash -d desktop="$(wslpath "$(/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe /C Write-Output '${Env:USERPROFILE}' | sed 's/\r//')")/Desktop"
+hash -d desktop="$(wslpath "$($powershell /C Write-Output '${Env:USERPROFILE}' | sed 's/\r//')")/Desktop"
 
 ln -sf /mnt/wslg/.X11-unix/X0 /tmp/.X11-unix/
