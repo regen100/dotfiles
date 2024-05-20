632ed36dc03d4ec52f1447b0b5795b54dca08a5f
Regen <regen100@users.noreply.github.com>
2024-05-20 13:37:55 +0900

    Add curl_time

--- zsh/.zshrc.d/70-func.zsh
+++ zsh/.zshrc.d/70-func.zsh
@@ -1,2 +1,14 @@
 _mkcd() { _wanted directories expl directory _path_files -/; }
 compdef _mkcd mkcd
+
+curl_time() {
+    curl -so /dev/null -w "\
+   namelookup:  %{time_namelookup}s\n\
+      connect:  %{time_connect}s\n\
+   appconnect:  %{time_appconnect}s\n\
+  pretransfer:  %{time_pretransfer}s\n\
+     redirect:  %{time_redirect}s\n\
+starttransfer:  %{time_starttransfer}s\n\
+-------------------------\n\
+        total:  %{time_total}s\n" "$@"
+}
